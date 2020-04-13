e9817245a3e57b8a0969103e379b573727e1fa2b
MwlLj <731025894@qq.com>
2020-04-13 18:57:59 +0800

    新增Test object

--- tdop_op/src/tdop_op.nim
+++ tdop_op/src/tdop_op.nim
@@ -1,11 +1,16 @@
 import "expression/parse"
 import "expression/express"
+import "../../lexical_analysis/src/token/token" as token
 import "../../lexical_analysis/src/token/parse" as token_parse
 import "../../lexical_analysis/src/token/handle" as token_handle
 import strformat
 import options
 
-proc iterExpr(value: express.ExprValue): int64 =
+type
+    Test = object
+        tokens: seq[token.Token]
+
+proc iterExpr(self: Test, value: express.ExprValue): int64 =
     if value.exp.isSome():
         var left, right: int64 = 0
         var op: char
@@ -15,10 +20,10 @@ proc iterExpr(value: express.ExprValue): int64 =
             echo(fmt"op: {op}")
         if exp.left.isSome():
             echo("left expr:")
-            left = iterExpr(exp.left.get())
+            left = self.iterExpr(exp.left.get())
         if exp.right.isSome():
             echo("right expr: ")
-            right = iterExpr(exp.right.get())
+            right = self.iterExpr(exp.right.get())
         case op
         of '+':
             return left + right
@@ -35,16 +40,24 @@ proc iterExpr(value: express.ExprValue): int64 =
         echo(v)
         return v
 
+proc parse(self: Test) =
+    var parser = parse.new(self.tokens)
+    let exprValue = parser.express(0)
+    if exprValue.isSome():
+        let r = self.iterExpr(exprValue.get())
+        echo(fmt"result: {r}")
+
+proc newTest(tokens: seq[token.Token]): Test =
+    result = Test(
+        tokens: tokens
+    )
+
 proc main() =
     let stream = readFile("./resource/test.dog")
     var tokenParser = token_parse.new(stream)
     let tokens = token_handle.parse(tokenParser)
-    echo(tokens)
-    var parser = parse.new(tokens)
-    let exprValue = parser.express(0)
-    if exprValue.isSome():
-        let r = iterExpr(exprValue.get())
-        echo(fmt"result: {r}")
+    var test = newTest(tokens)
+    test.parse()
 
 when isMainModule:
     main()
